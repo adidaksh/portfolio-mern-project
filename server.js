0d0194817b0ee341ab08bcf720e83529cc9e44e3
adityadaksh31 <124888390+adidaksh@users.noreply.github.com>
2025-01-03 22:38:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.use (express.static(path.join(__dirname, 'build')));
 
-routes
+//routes
 app.use('/api/v1/portfolio',require('./routes/portfolioRoutes'));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
